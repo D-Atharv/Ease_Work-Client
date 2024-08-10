a6949fe64f5ef28ddc9caf34b5798be3fdfdc321
D-Atharv <atharvdewanganakd298suncity@gmail.com>
2024-08-10 12:41:26 +0530

    inspection from + voice added

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="bg-slate-300 w-full  flex flex-col justify-center items-center h-screen">
       <h1 className="text-4xl font-bold mb-6">Welcome to Ease Work Client</h1>
-      <Link href="/text-generator" className="text-blue-500 hover:underline">
+      <Link href="/textGenerator" className="text-blue-500 text-2xl font-bold mb-6 hover:underline">
         Go to Voice to Text Converter
       </Link>
     </div>
